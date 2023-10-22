c56b2d4abf622c2a4d27ea4b5b3d1988d69bcbd0
aesthaltics <aesthaltics@pm.me>
2023-10-22 11:07:58 +0200

    added colors and minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { randomInt } from "crypto";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 import dictionary from "./dictionary.json";
+import { ForceGraphMethods } from "react-force-graph-2d";
 
 type node = {
 	id: string;
@@ -50,54 +51,51 @@ const createWordNode = (word: string) => {
 };
 
 export default function Home() {
-	const [data, setData] = useState<{nodes: node[], links: link[]}>({nodes: [], links: []});
+	const [data, setData] = useState<{ nodes: node[]; links: link[] }>({
+		nodes: [],
+		links: [],
+	});
 	let graphContainer = useRef<HTMLDivElement>(null);
 	let wordList = useRef(Object.keys(dictionary));
 	const addedWords = useRef(new Set());
+	const graphRef = useRef<ForceGraphMethods>()
 
-	const addNodes = (nodes: node[]) => {
-		setData((oldData) => {
-			const newNodes = [...oldData.nodes, ...nodes]
-			console.log(`new node list: ${JSON.stringify(newNodes)}`)
-			return {
-				...oldData,
-				nodes: newNodes
-			};
-		});
-	};
 	const addLinks = (links: link[]) => {
 		setData((oldData) => {
-			const updatedNodes = oldData.nodes
-			const newLinks = [...oldData.links]
-			links.forEach(link => {
-				console.log(`trying to add link: ${JSON.stringify(link)}`)
-				const nodeIndex = updatedNodes.findIndex(node => node.id === link.target)
-				if (nodeIndex > -1){
-					updatedNodes[nodeIndex].val += 1
-					newLinks.push(link)
+			const updatedNodes = oldData.nodes;
+			const newLinks = [...oldData.links];
+			links.forEach((link) => {
+				// console.log(`trying to add link: ${JSON.stringify(link)}`);
+				const nodeIndex = updatedNodes.findIndex(
+					(node) => node.id === link.target
+				);
+				if (nodeIndex > -1) {
+					updatedNodes[nodeIndex].val += 1;
+					newLinks.push(link);
 				} else {
-					console.log(`Couldn't find ${link.target} node`)
+					// console.log(`Couldn't find ${link.target} node`);
 				}
-				return
-			})
-			console.log(`updatedNodes: ${JSON.stringify(updatedNodes)}`)
-			console.log(`links: ${JSON.stringify(newLinks)}`)
+				return;
+			});
+			// console.log(`updatedNodes: ${JSON.stringify(updatedNodes)}`);
+			// console.log(`links: ${JSON.stringify(newLinks)}`);
 			return {
 				nodes: [...updatedNodes],
 				links: [...newLinks],
 			};
 		});
 	};
-	const addWord = (word?: string) => {
-		if (word === undefined){
-			return
+	const addWord = useCallback(() => {
+		const word = wordList.current.pop()
+		if (word === undefined) {
+			return;
 		}
-		const wordsToAdd: node[] = []
-		const linksToAdd: link[] = []
+		const wordsToAdd: node[] = [];
+		const linksToAdd: link[] = [];
 		if (!addedWords.current.has(word.toLowerCase())) {
-			console.log(`adding ${word.toLowerCase()}`)
-			wordsToAdd.push(createWordNode(word.toLowerCase()))
-			addedWords.current.add(word.toLowerCase())
+			// console.log(`adding ${word.toLowerCase()}`);
+			wordsToAdd.push(createWordNode(word.toLowerCase()));
+			addedWords.current.add(word.toLowerCase());
 		}
 		const wordDescription = typedDict[word]
 			.replaceAll(/[^a-zA-Z ]+/g, "")
@@ -106,23 +104,47 @@ export default function Home() {
 		wordDescription.forEach((descriptionWord) => {
 			if (!addedWords.current.has(descriptionWord)) {
 				const newNode = createWordNode(descriptionWord);
-				wordsToAdd.push(newNode)
-				addedWords.current.add(descriptionWord)
+				wordsToAdd.push(newNode);
+				addedWords.current.add(descriptionWord);
 			}
-			const newLinks = createLinks(word.toLowerCase(), [descriptionWord])
-			linksToAdd.push(...newLinks)
+			const newLinks = createLinks(word.toLowerCase(), [descriptionWord]);
+			linksToAdd.push(...newLinks);
 		});
-		addNodes(wordsToAdd)
-		addLinks(linksToAdd)
-	};
+		addNodes(wordsToAdd);
+		addLinks(linksToAdd);
+	}, []);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			addWord();
+		}, 500);
+		return () => {
+			clearInterval(interval);
+		}
+	}, []);
 
+	const addNodes = (nodes: node[]) => {
+		setData((oldData) => {
+			const newNodes = [...oldData.nodes, ...nodes];
+			// console.log(`new node list: ${JSON.stringify(newNodes)}`);
+			return {
+				...oldData,
+				nodes: newNodes,
+			};
+		});
+	};
 	return (
 		<div className="flex flex-col max-h-screen max-w-screen outline h-screen w-screen">
 			<div className="flex flex-col items-center justify-center h-full max-h-full">
 				<div className="flex-none max-h-full">
-					<button className="bg-blue-500 px-4 py-2" onClick={() => addWord(wordList.current.pop())}>
+					<button
+						className="bg-blue-500 px-4 py-2"
+						onClick={() => addWord()}
+					>
 						Click Me!
 					</button>
+					<p className="text-lg">
+						Words added so far {addedWords.current.size}
+					</p>
 				</div>
 				<div
 					className="grow basis-0 flex-col w-full max-w-full min-h-0 max-h-full"
@@ -131,6 +153,16 @@ export default function Home() {
 					<ForceGraph2D
 						graphData={data}
 						height={graphContainer.current?.clientHeight}
+						nodeAutoColorBy={"val"}
+						// linkDirectionalParticles={1}
+						// linkDirectionalParticleSpeed={0.003}
+						// linkDirectionalParticleColor={() => "white"}
+						linkAutoColorBy={"white"}
+						// backgroundColor={"black"}
+						nodeLabel={"name"}
+						cooldownTicks={100}
+						d3AlphaDecay={0}
+						d3VelocityDecay={0}
 					/>
 				</div>
 			</div>
