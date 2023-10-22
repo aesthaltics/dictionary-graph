12c8b6acda17b39a1ba0f16a152960e723c70d18
aesthaltics <aesthaltics@pm.me>
2023-10-22 06:34:41 +0200

    Append nodes and links to graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,46 @@
-"use client"
+"use client";
 
+import { randomInt } from "crypto";
+import { useState, useRef } from "react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
-const ForceGraph2D = dynamic(() => import('react-force-graph-2d'), {ssr: false})
+const ForceGraph2D = dynamic(() => import("react-force-graph-2d"), {
+	ssr: false,
+});
+
+const createNode = (linkTo: string[], linkFrom: string[]) => {
+	let node = [];
+	let links: { source: string; target: string }[] = [];
+
+	const number = Math.round(Math.random() * 1000).toString(10);
+
+	const id = "id".concat(number);
+	const name = "name".concat(number);
+	const val = Math.round(Math.random() * 10)
+
+	node.push({
+		id: id,
+		name: name,
+		val: val,
+	});
+	linkTo.forEach((targetId) =>
+		links.push({
+			source: id,
+			target: targetId,
+		})
+	);
+	linkFrom.forEach((sourceId) =>
+		links.push({
+			source: sourceId,
+			target: id,
+		})
+	);
+	console.log(`node: ${JSON.stringify(node)}`)
+	console.log(`links: ${JSON.stringify(links)}`);
+
+
+	return { node, links };
+};
 
 const myData = {
 	nodes: [
@@ -26,9 +64,44 @@ const myData = {
 };
 
 export default function Home() {
+	const [data, setData] = useState(myData);
+	let graphContainer = useRef<HTMLDivElement>(null)
+
+	const addNode = () => {
+		const linkTo: string[] = []
+		const linkFrom: string[] = []
+		data.nodes.forEach(node => {
+			if (Math.random() > 0.5){
+				return linkTo.push(node.id)
+			}
+			return linkFrom.push(node.id)
+		})
+		const { node, links } = createNode(linkTo, linkFrom);
+		setData((oldData) => {
+			return {
+				nodes: [...oldData.nodes, ...node],
+				links: [...oldData.links, ...links]
+			};
+		});
+		console.log(data)
+	};
+
 	return (
-		<div className="flex flex-col items-center justify-center">
-			<ForceGraph2D graphData={myData} />
+		<div className="flex flex-col max-h-screen max-w-screen outline h-screen w-screen">
+			<div className="flex flex-col items-center justify-center h-full max-h-full">
+				<div className="flex-none max-h-full">
+					<button className="bg-blue-500 px-4 py-2" onClick={addNode}>Click Me!</button>
+				</div>
+				<div
+					className="grow basis-0 flex-col w-full max-w-full min-h-0 max-h-full"
+					ref={graphContainer}
+				>
+					<ForceGraph2D
+						graphData={data}
+						height={graphContainer.current?.clientHeight}
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
